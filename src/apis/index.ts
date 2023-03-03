0bac2d514190390a3f27d3b208a361e774e28fae
jaeyeong815 <jae0ee@naver.com>
2023-03-04 01:09:18 +0900

    [REFACTOR] api 요청 방법 수정

---
+++ src/apis/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const questionGPT = (question: string) =>
+  axios
+    .post(
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        prompt: question,
+        temperature: 0.4,
+        max_tokens: 1888,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.choices[0].text;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
--- src/pages/api/index.ts
+++
@@ -1,4 +0,0 @@
-import instance from '@/pages/api/instance/axiosInstance';
-
-export const foodRecommend = (selectList: string) => instance.post('/food', { ingredient: selectList });
-export const searchRecipe = (food: string) => instance.post('/recipe', { food });
